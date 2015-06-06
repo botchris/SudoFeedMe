47bbfc1bfba36a102bfb531f1ebc122edf5234a0
Christopher Castro <chris@quickapps.es>
2015-06-06 19:16:06 +0200

    validation rules

--- src/Model/Table/IssuesTable.php
+++ src/Model/Table/IssuesTable.php
@@ -1,13 +1,9 @@
 <?php
 namespace App\Model\Table;
 
-use Cake\Event\Event;
-use Cake\ORM\Entity;
-use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
-use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
-use \ArrayObject;
 
 /**
  * Represents "issues" database table.
@@ -16,17 +12,31 @@ use \ArrayObject;
 class IssuesTable extends Table
 {
 
-    /**
-     * Initialize a table instance. Called after the constructor.
-     *
-     * @param array $config Configuration options passed to the constructor
-     * @return void
-     */
     public function initialize(array $config)
     {
         $this->belongsTo('Track', [
             'className' => 'Tracks',
             'propertyName' => 'track',
         ]);
+    }
+
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn('track_id', 'tracks'), 'trackExists', [
+            'message' => 'Pista inválida'
+        ]);
+
+        return $rules;
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('description', 'validDescription', [
+                'rule' => 'notBlank',
+                'message' => __('You need to provide a valid role'),
+                'provider' => 'table',
+            ]);
+        return $validator;
     }
 }
